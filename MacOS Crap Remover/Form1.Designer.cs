280375c439a2b7f7f121e783e1838e183c9521e3
Hamish Scott <Hamish_Scott@outlook.com>
2020-10-29 22:50:54 +1100

    init

--- MacOS Crap Remover/Form1.Designer.cs
+++ MacOS Crap Remover/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.select_directory = new System.Windows.Forms.Button();
+            this.location_display = new System.Windows.Forms.TextBox();
+            this.select_text = new System.Windows.Forms.Label();
+            this.run_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // select_directory
+            // 
+            this.select_directory.Location = new System.Drawing.Point(13, 71);
+            this.select_directory.Name = "select_directory";
+            this.select_directory.Size = new System.Drawing.Size(75, 23);
+            this.select_directory.TabIndex = 0;
+            this.select_directory.Text = "Select...";
+            this.select_directory.UseVisualStyleBackColor = true;
+            this.select_directory.Click += new System.EventHandler(this.select_directory_Click);
+            // 
+            // location_display
+            // 
+            this.location_display.Location = new System.Drawing.Point(13, 45);
+            this.location_display.Name = "location_display";
+            this.location_display.Size = new System.Drawing.Size(246, 20);
+            this.location_display.TabIndex = 1;
+            // 
+            // select_text
+            // 
+            this.select_text.AutoSize = true;
+            this.select_text.Location = new System.Drawing.Point(13, 26);
+            this.select_text.Name = "select_text";
+            this.select_text.Size = new System.Drawing.Size(100, 13);
+            this.select_text.TabIndex = 2;
+            this.select_text.Text = "Select File Location";
+            // 
+            // run_button
+            // 
+            this.run_button.Location = new System.Drawing.Point(183, 70);
+            this.run_button.Name = "run_button";
+            this.run_button.Size = new System.Drawing.Size(75, 23);
+            this.run_button.TabIndex = 3;
+            this.run_button.Text = "Run";
+            this.run_button.UseVisualStyleBackColor = true;
+            this.run_button.Click += new System.EventHandler(this.run_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.run_button);
+            this.Controls.Add(this.select_text);
+            this.Controls.Add(this.location_display);
+            this.Controls.Add(this.select_directory);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button select_directory;
+        private System.Windows.Forms.TextBox location_display;
+        private System.Windows.Forms.Label select_text;
+        private System.Windows.Forms.Button run_button;
     }
 }
 
--- MacOS Crap Remover/Form1.cs
+++ MacOS Crap Remover/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,87 @@ namespace MacOS_Crap_Remover
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /*
+         * 
+         * cd /location/location/etc
+            f:
+
+            del /s /q /f /a .DS_STORE
+            del /s /q /f /a .Trashes
+            del /s /q /f /a ._.*
+            del /s /q /f /a ._*
+
+        string del_cmd1 = "";
+        string del_cmd2 = "";
+        string del_cmd3 = "";
+        string del_cmd4 = "";
+         */
+
+
+
+        FolderBrowserDialog outputBrowser = new FolderBrowserDialog();
+        private string del_cmd1 = "del /s /q /f /a .DS_STORE";
+        private string del_cmd2 = "del /s /q /f /a .Trashes";
+        private string del_cmd3 = "del /s /q /f /a ._.*";
+        private string del_cmd4 = "del /s /q /f /a ._*";
+
+        private string selected_location = "D:\\User\\Documents\\Tools etc\\Autoruns";
+
+        private void main() {
+
+        }
+
+        private void set_cmd()
+        {
+
+        }
+
+        private void run_cmds()
+        {
+            Process StartCMD = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Normal/*Normal or Hidden*/;
+            startInfo.FileName = "cmd.exe";
+
+            //startInfo.WorkingDirectory = @"C:\"; //how to
+
+            StartCMD.StartInfo = startInfo;
+
+            startInfo.Arguments = selected_location;
+
+            //startInfo.Arguments = del_cmd1;
+            //startInfo.Arguments = del_cmd2;
+            //startInfo.Arguments = del_cmd3;
+            //startInfo.Arguments = del_cmd4;
+
+
+            StartCMD.Start();
+            StartCMD.WaitForExit();
+        }
+
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void select_directory_Click(object sender, EventArgs e)
+        {
+            outputBrowser.SelectedPath = selected_location;
+            outputBrowser.Description = "Select Output Folder";
+
+            //if (outputBrowser.ShowDialog() == CommonFileDialogResult.Ok)
+            //{
+            //    location_display.Text = outputBrowser.;
+            //}
+        }
+
+        private void run_button_Click(object sender, EventArgs e)
+        {
+            run_cmds();
         }
     }
 }
